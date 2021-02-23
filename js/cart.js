50a38251be40b60f3030212a1864867601d1daee
febianodja <febianodja@gmail.com>
2021-02-24 02:07:19 +0700

    edit cart html & js

--- js/cart.js
+++ js/cart.js
@@ -23,14 +23,6 @@
 // };
 
 // showCartUser();
-const getAddOns = async () => {
-  const api = `https://6023a8ba6bf3e6001766b52c.mockapi.io/add-ons`;
-  let response = await fetch(api);
-  let result = await response.json();
-
-  console.log(result);
-}
-getAddOns();
 
 const getProducts = async () => {
   let dataProduct = localStorage.getItem("productId")
@@ -38,23 +30,81 @@ const getProducts = async () => {
   let response = await fetch(api);
   let result = await response.json();
   console.log(result);
-  let display = document.querySelector("#tampil");
+  let itemDetail =""
+  for(let i = 0 ; i < result.detail.length ; i++){
+    itemDetail += "<li>"+result.detail[i]+"</li>";
+    
+  }
+  let display = document.querySelector("#cartTampil");
   let showDisplay = document.createElement("div");
+  let clickedBtn = document.querySelector("#checkoutBtn")
+  clickedBtn.addEventListener("click", () => {
+    localStorage.setItem("packageName", JSON.stringify(`${result.produk}`))
+    localStorage.setItem("packagePrice", JSON.stringify(`${result.price}`))
+  })
   
-  showDisplay.innerHTML=`<h3>${result.produk}</h3>
-  <p>${result.detail}</p>
-  <h3>${result.price}</h3>
+  showDisplay.innerHTML=`<h3 id="cartProduct">${result.produk}</h3>
+  <ul>${itemDetail}</ul> 
+  <br>
+  <h3>Rp.${result.price}</h3>`
   
-
-  <button type="button" onclick="isClicked(${result.price})" class="btn btn-secondary"><a href="./payment.html">CHECKOUT</a></button>`
-
   
   
   display.appendChild(showDisplay)
 }
 getProducts();
 
-const isClicked = (price) => {
-  localStorage.setItem("totalPrice", JSON.stringify(price));
+const getAddOns = async () => {
+  const api = `https://6023a8ba6bf3e6001766b52c.mockapi.io/add-ons`;
+  let response = await fetch(api);
+  let result = await response.json();
+  console.log(result);
+  result.map(item => {
+
+    let display = document.querySelector("#showAddOns");
+    let showDisplay = document.createElement("div");
+    // let clickedBtn = document.querySelector("#checkoutBtn")
+    // clickedBtn.addEventListener("click", () => {
+    //   let checkedBox = document.getElementsByClassName(".form-check-input:checked");
+    //   console.log(checkedBox);
+    // })
+    
+    showDisplay.innerHTML = `
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="${item.id}" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+    ${item.extra} (Rp.${item.extraPrice})
+    </label>
+    </div>`
+    
+    display.appendChild(showDisplay)
+  })
 }
+getAddOns();
+
+// const isClicked = (price) => {
+  //   localStorage.setItem("totalPrice", JSON.stringify(price));
+  // }
+  
+  
+  // let checkedValue = document.querySelector('.form-check-input').value;
+  // checkedValue.addEventListener("")
+  // console.log(checkedValue);
+  
+  // let checkboxValue = document.getElementsByClassName(".form-check-input").value
+  // console.log(checkboxValue);
+
+  function getValue(){
+    let checked = document.getElementsByClassName("form-check-input");
+    let checkedValue = [];
+    for(let i = 0 ; i < 3; i++){
+      if(checked[i].checked === true){
+        checkedValue.push(checked[i].value);
+        localStorage.setItem("idAddOns", checkedValue)
+      }
+    }
+    
+  }
+
+  
 
